a96ede1cdd7cf18a13fa97b98267b24a22a8d744
Chetan M  Kittali <102372671+Neo28A@users.noreply.github.com>
2025-03-21 21:28:58 +0530

    Meet Neo

---
+++ project/app/essays/essays.tsx
@@ -0,0 +1,7 @@
+export default function Essays() {
+  return (
+    <main className="flex items-center justify-center min-h-screen bg-[#fdfff4]">
+      <h1 className="text-2xl font-bold text-muted-foreground">Essays - Under Development</h1>
+    </main>
+  );
+} 
